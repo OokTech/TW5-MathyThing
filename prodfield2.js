b15ebca125f63ce8e4dc6533cb47f007c7e2bf8d
inmysocks <inmysocks@gmail.com>
2014-12-28 23:23:59 -0700

    Added prodfield2.js, fixed some indenting in prodfield.js
    Prodfield2.js takes the product of two given fields and stores it in a third field.
    It does this in all tiddlers in the result of a filter.

--- prodfield2.js
+++ prodfield2.js
@@ -1,11 +1,11 @@
 /*\
-title: $:/plugins/inymsocks/MathyThing/prodfield.js
+title: $:/plugins/inymsocks/MathyThing/prodfield2.js
 type: application/javascript
 module-type: widget
 
 Multiply the values in a specified field and store the result in another field
 
-<$prodfield filter='[tag[prod]]' prodfield='prod_field' storefield='store_field'/>
+<$prodfield2 filter='[tag[prod]]' prodfield='prod_field' prodfield2='prod_field2' storefield='store_field'/>
 
 This is an edited version of the list widget from TiddlyWiki5
 
@@ -18,7 +18,7 @@ This is an edited version of the list widget from TiddlyWiki5
 
 var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
-var ProdFieldWidget = function(parseTreeNode,options) {
+var ProdFieldWidget2 = function(parseTreeNode,options) {
 	// Main initialisation inherited from widget.js
 	this.initialise(parseTreeNode,options);
 };
@@ -26,12 +26,12 @@ var ProdFieldWidget = function(parseTreeNode,options) {
 /*
 Inherit from the base widget class
 */
-ProdFieldWidget.prototype = new Widget();
+ProdFieldWidget2.prototype = new Widget();
 
 /*
 Render this widget into the DOM
 */
-ProdFieldWidget.prototype.render = function(parent,nextSibling) {
+ProdFieldWidget2.prototype.render = function(parent,nextSibling) {
 	this.computeAttributes();
 	this.execute();
 };
@@ -39,18 +39,15 @@ ProdFieldWidget.prototype.render = function(parent,nextSibling) {
 /*
 Compute the internal state of the widget
 */
-ProdFieldWidget.prototype.execute = function() {
+ProdFieldWidget2.prototype.execute = function() {
 	// Get attributes
-	this.actionTiddler = this.getAttribute("tiddler",this.getVariable("currentTiddler"));
 	this.prodField = this.getAttribute("prodfield");
+	this.prodField2 = this.getAttribute("prodfield2");
 	this.storeField = this.getAttribute("storefield");
 	this.storeIndex = this.getAttribute("index");
 	this.defaultValue = this.getAttribute("defaultvalue",0);
 	// Compose the list elements
 	this.list = this.getTiddlerList();
-	// Get current value
-	var storetiddler = this.wiki.getTiddler(this.actionTiddler);
-	var currentState = storetiddler.getFieldString(this.storeField);
 	// Check for an empty list, if the list isn't empty compute the product
 	if(this.list.length === 0) {
 		output = this.defaultValue; //return the default value when there is nothing to product, if it isn't set than return 0
@@ -60,50 +57,49 @@ ProdFieldWidget.prototype.execute = function() {
 			var tidtitle = this.list[i];
 			var tiddler = this.wiki.getTiddler(tidtitle);
 			// check to make sure that the field contains a number before using it in the product
-			if ( !isNaN(parseFloat(tiddler.getFieldString(this.prodField))) && isFinite(tiddler.getFieldString(this.prodField)) ) {
-				output = output * Number(tiddler.getFieldString(this.prodField));
+			if ( !isNaN(parseFloat(tiddler.getFieldString(this.prodField))) && isFinite(tiddler.getFieldString(this.prodField)) && !isNaN(parseFloat(tiddler.getFieldString(this.prodField2))) && isFinite(tiddler.getFieldString(this.prodField2)) ) {
+				output = Number(tiddler.getFieldString(this.prodField)) * Number(tiddler.getFieldString(this.prodField2));
+			}
+			 // If the product has changed then write to the field
+			this.output = String(output);
+			if (this.output === String(tiddler.getFieldString(this.storeField))) {
+			} else {
+				this.wiki.setText(tidtitle,this.storeField,this.storeIndex,this.output);
 			}
 		}
-	}
-        // If the product has changed then write to the field
-	this.output = String(output);
-	if (this.output === String(storetiddler.getFieldString(this.storeField))) {
-	} else {
-		this.wiki.setText(this.actionTiddler,this.storeField,this.storeIndex,this.output);
 	}
 };
 
 /*
 Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
 */
-ProdFieldWidget.prototype.refresh = function(changedTiddlers) {
+ProdFieldWidget2.prototype.refresh = function(changedTiddlers) {
 	var changedAttributes = this.computeAttributes();
 	var output = 1;
 	this.list = this.getTiddlerList();
 	for (var i = 0; i < this.list.length; i++) {
 		var tidtitle = this.list[i];
 		var tiddler = this.wiki.getTiddler(tidtitle);
-		if ( !isNaN(parseFloat(tiddler.getFieldString(this.prodField))) && isFinite(tiddler.getFieldString(this.prodField)) ) {
-	  		output = output * Number(tiddler.getFieldString(this.prodField));
+		if ( !isNaN(parseFloat(tiddler.getFieldString(this.prodField))) && isFinite(tiddler.getFieldString(this.prodField)) && !isNaN(parseFloat(tiddler.getFieldString(this.prodField2))) && isFinite(tiddler.getFieldString(this.prodField2)) ) {
+	  		output = Number(tiddler.getFieldString(this.prodField)) * Number(tiddler.getFieldString(this.prodField2));
 		}
-	}
-	var storetiddler = this.wiki.getTiddler(this.actionTiddler);
-	// Completely rerender if any of our attributes have changed
-	if (String(output) != String(storetiddler.getFieldString(this.storeField))) {
-		this.refreshSelf();
-		return true;
-	} else if(this.stateTitle && changedTiddlers[this.stateTitle]) {
-		this.readState();
-		return true;
+		// Completely rerender if any of our attributes have changed
+		if (String(output) != String(tiddler.getFieldString(this.storeField))) {
+			this.refreshSelf();
+			return true;
+		} else if(this.stateTitle && changedTiddlers[this.stateTitle]) {
+			this.readState();
+			return true;
+		}
 	}
 	return false;
 };
 
-ProdFieldWidget.prototype.getTiddlerList = function() {
+ProdFieldWidget2.prototype.getTiddlerList = function() {
 	var defaultFilter = "[!is[system]is[system]]"; // this will always return an empty list and is always valid, so default behavior is to return an empty list
 	return this.wiki.filterTiddlers(this.getAttribute("filter",defaultFilter),this);
 };
 
-exports.prodfield = ProdFieldWidget;
+exports.prodfield2 = ProdFieldWidget2;
 
 })();
