9048e791d40312d1812a6646bf648acacddfecef
inmysocks <inmysocks@gmail.com>
2014-12-29 23:02:46 -0700

    Added sumfield2 and updated readme and some small typos
    sumfield2 sums two specific fields in each tiddler in a list and stores each value instead of summing the same field across all the tiddlers in a list.

--- README.md
+++ README.md
@@ -11,20 +11,66 @@ Currently this plugin contains:
 
 Usage:
 
-<$action-storecount $filter=<<someFilter>> $tiddler=someTiddler $field=someField/>
+`<$action-storecount $filter=<<someFilter>> $tiddler=someTiddler $field=someField/>`
 
 Since it is a modification of the action-setfield widget and I haven't modifed this part, you may be able to store the output at the given index of a data tiddler, but I haven't tested that yet.
 
 ---
 
-THIS WIDGET SHOULDN'T BE USED FOR THE MOMENT. It constantly refreshes and will slow down a wiki whenever a tiddler with this widget is rendered. If you have any suggestions or help on how to fix this I would greatly appreciate it.
 
 ''The sumfield widget'' - a widget that was made by modifying the list widget. It takes a filter and a given field and sums together everything in that field in the filtered tiddlers.
 
 Usage:
 
-<$sumfield filter=<<someFilter>> sumfield=sumField tiddler=storeTiddler storefield=storeField defaultvalue=defaultValue/>
+`<$sumfield filter=<<someFilter>> sumfield=sumField tiddler=storeTiddler storefield=storeField defaultvalue=defaultValue/>`
 
-It will take each tiddler listed when using <<someFilter>> and take the value in sumField from each of the tiddlers and sum them, the result will be placed in the storeField of the tiddler storeTiddler. If there aren't any numbers to sum than it will display defaultValue.
+It will take each tiddler listed when using `<<someFilter>>` and take the value in sumField from each of the tiddlers and sum them, the result will be placed in the storeField of the tiddler storeTiddler. If there aren't any numbers to sum than it will display defaultValue.
 
-tiddler defaults to <<currentTiddler>> and defaultValue defaults to 0 if they aren't given inputs.
+tiddler defaults to `<<currentTiddler>>` and defaultValue defaults to 0 if they aren't given inputs.
+
+If one of the fields contains a non-numeric value than it is ignored. This includes empty fields.
+
+---
+
+
+''The sumfield2 widget'' - a widget that was made by modifying the list widget. It takes a filter and a given field and sums together two fields in the filtered tiddlers and stores the value in a third field.
+
+Usage:
+
+`<$sumfield2 filter=<<someFilter>> sumfield=sumField sumfield2=sumField2 storefield=storeField defaultvalue=defaultValue/>`
+
+It will take each tiddler listed when using `<<someFilter>>` and take the value in sumField to the value in sumField2 in each of the tiddlers and the result will be placed in the storeField of each tiddler. If there aren't any numbers to sum than it will display defaultValue.
+
+tiddler defaults to `<<currentTiddler>>` and defaultValue defaults to 0 if they aren't given inputs.
+
+If one of the fields contains a non-numeric value than it is ignored. This includes empty fields.
+
+---
+
+
+''The prodfield widget'' - a widget that was made by modifying the list widget. It takes a filter and a given field and takes the product of everything in that field in the filtered tiddlers.
+
+Usage:
+
+`<$prodfield filter=<<someFilter>> prodfield=prodField tiddler=storeTiddler storefield=storeField defaultvalue=defaultValue/>`
+
+It will take each tiddler listed when using `<<someFilter>>` and take the value in prodField from each of the tiddlers and take their product, the result will be placed in the storeField of the tiddler storeTiddler. If there aren't any numbers to sum than it will display defaultValue. 
+
+tiddler defaults to `<<currentTiddler>>` and defaultValue defaults to 0 if they aren't given inputs.
+
+If one of the fields contains a non-numeric value than it is ignored. This includes empty fields.
+
+---
+
+
+''The prodfield2 widget'' - a widget that was made by modifying the list widget. It takes a filter and a given two fields takes the product of those fields in the filtered tiddlers, then stores the result in a third field in each tiddler.
+
+Usage:
+
+`<$prodfield2 filter=<<someFilter>> prodfield=prodField prodfield2=prodField2 storefield=storeField defaultvalue=defaultValue/>`
+
+It will take each tiddler listed when using `<<someFilter>>` and multiply the values in prodField and prodField2 in each of the tiddlers and the result will be placed in the storeField of each tiddler. If there aren't any numbers to sum than it will display defaultValue. 
+
+tiddler defaults to `<<currentTiddler>>` and defaultValue defaults to 0 if they aren't given inputs.
+
+If one of the fields contains a non-numeric value than it is ignored. This includes empty fields.
--- sumfield2.js
+++ sumfield2.js
@@ -1,11 +1,11 @@
 /*\
-title: $:/plugins/inymsocks/MathyThing/prodfield2.js
+title: $:/plugins/inymsocks/MathyThing/sumfield2.js
 type: application/javascript
 module-type: widget
 
-Multiply the values in two specified fields and store the result in another field
+Add the values in two specified fields and store the result in another field
 
-<$prodfield2 filter='[tag[prod]]' prodfield='prod_field' prodfield2='prod_field2' storefield='store_field'/>
+<$sumfield2 filter='[tag[sum]]' sumfield='sum_field' sumfield2='sum_field2' storefield='store_field'/>
 
 This is an edited version of the list widget from TiddlyWiki5
 
@@ -18,7 +18,7 @@ This is an edited version of the list widget from TiddlyWiki5
 
 var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
-var ProdFieldWidget2 = function(parseTreeNode,options) {
+var SumFieldWidget2 = function(parseTreeNode,options) {
 	// Main initialisation inherited from widget.js
 	this.initialise(parseTreeNode,options);
 };
@@ -26,12 +26,12 @@ var ProdFieldWidget2 = function(parseTreeNode,options) {
 /*
 Inherit from the base widget class
 */
-ProdFieldWidget2.prototype = new Widget();
+SumFieldWidget2.prototype = new Widget();
 
 /*
 Render this widget into the DOM
 */
-ProdFieldWidget2.prototype.render = function(parent,nextSibling) {
+SumFieldWidget2.prototype.render = function(parent,nextSibling) {
 	this.computeAttributes();
 	this.execute();
 };
@@ -39,28 +39,28 @@ ProdFieldWidget2.prototype.render = function(parent,nextSibling) {
 /*
 Compute the internal state of the widget
 */
-ProdFieldWidget2.prototype.execute = function() {
+SumFieldWidget2.prototype.execute = function() {
 	// Get attributes
-	this.prodField = this.getAttribute("prodfield");
-	this.prodField2 = this.getAttribute("prodfield2");
+	this.sumField = this.getAttribute("sumfield");
+	this.sumField2 = this.getAttribute("sumfield2");
 	this.storeField = this.getAttribute("storefield");
 	this.storeIndex = this.getAttribute("index");
 	this.defaultValue = this.getAttribute("defaultvalue",0);
 	// Compose the list elements
 	this.list = this.getTiddlerList();
-	// Check for an empty list, if the list isn't empty compute the product
+	// Check for an empty list, if the list isn't empty compute the sum
 	if(this.list.length === 0) {
-		output = this.defaultValue; //return the default value when there is nothing to product, if it isn't set than return 0
+		output = this.defaultValue; //return the default value when there is nothing to sum, if it isn't set than return 0
 	} else {
 		for (var i = 0; i < this.list.length; i++) {
 			var output = 1;
 			var tidtitle = this.list[i];
 			var tiddler = this.wiki.getTiddler(tidtitle);
-			// check to make sure that the field contains a number before using it in the product
-			if ( !isNaN(parseFloat(tiddler.getFieldString(this.prodField))) && isFinite(tiddler.getFieldString(this.prodField)) && !isNaN(parseFloat(tiddler.getFieldString(this.prodField2))) && isFinite(tiddler.getFieldString(this.prodField2)) ) {
-				output = Number(tiddler.getFieldString(this.prodField)) * Number(tiddler.getFieldString(this.prodField2));
+			// check to make sure that the field contains a number before using it in the sum
+			if ( !isNaN(parseFloat(tiddler.getFieldString(this.sumField))) && isFinite(tiddler.getFieldString(this.sumField)) && !isNaN(parseFloat(tiddler.getFieldString(this.sumField2))) && isFinite(tiddler.getFieldString(this.sumField2)) ) {
+				output = Number(tiddler.getFieldString(this.sumField)) + Number(tiddler.getFieldString(this.sumField2));
 			}
-			 // If the product has changed then write to the field
+			 // If the sum has changed then write to the field
 			this.output = String(output);
 			if (this.output === String(tiddler.getFieldString(this.storeField))) {
 			} else {
@@ -73,15 +73,15 @@ ProdFieldWidget2.prototype.execute = function() {
 /*
 Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
 */
-ProdFieldWidget2.prototype.refresh = function(changedTiddlers) {
+SumFieldWidget2.prototype.refresh = function(changedTiddlers) {
 	var changedAttributes = this.computeAttributes();
 	this.list = this.getTiddlerList();
 	for (var i = 0; i < this.list.length; i++) {
 		var output = 1;
 		var tidtitle = this.list[i];
 		var tiddler = this.wiki.getTiddler(tidtitle);
-		if ( !isNaN(parseFloat(tiddler.getFieldString(this.prodField))) && isFinite(tiddler.getFieldString(this.prodField)) && !isNaN(parseFloat(tiddler.getFieldString(this.prodField2))) && isFinite(tiddler.getFieldString(this.prodField2)) ) {
-	  		output = Number(tiddler.getFieldString(this.prodField)) * Number(tiddler.getFieldString(this.prodField2));
+		if ( !isNaN(parseFloat(tiddler.getFieldString(this.sumField))) && isFinite(tiddler.getFieldString(this.sumField)) && !isNaN(parseFloat(tiddler.getFieldString(this.sumField2))) && isFinite(tiddler.getFieldString(this.sumField2)) ) {
+	  		output = Number(tiddler.getFieldString(this.sumField)) + Number(tiddler.getFieldString(this.sumField2));
 		}
 		// Completely rerender if any of our attributes have changed
 		if (String(output) != String(tiddler.getFieldString(this.storeField))) {
@@ -95,11 +95,11 @@ ProdFieldWidget2.prototype.refresh = function(changedTiddlers) {
 	return false;
 };
 
-ProdFieldWidget2.prototype.getTiddlerList = function() {
+SumFieldWidget2.prototype.getTiddlerList = function() {
 	var defaultFilter = "[!is[system]is[system]]"; // this will always return an empty list and is always valid, so default behavior is to return an empty list
 	return this.wiki.filterTiddlers(this.getAttribute("filter",defaultFilter),this);
 };
 
-exports.prodfield2 = ProdFieldWidget2;
+exports.sumfield2 = SumFieldWidget2;
 
 })();
