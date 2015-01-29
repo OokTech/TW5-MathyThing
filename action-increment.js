bb5206f9a73d9f160d9fde41ba5ca0e4109063d3
inmysocks <inmysocks@gmail.com>
2015-01-28 18:05:28 -0700

    Added ActionIncrementWidget.tid with examples of the action-increment widget and cleaned up the code for action-increment.js
    to possibly put in a pull request to add action-increment to the core

--- action-increment.js
+++ action-increment.js
@@ -5,32 +5,7 @@ module-type: widget
 
 Action widget that increments a number in a field.
 
-<$action-increment $tiddler=someTiddler $field=fieldToIncrement $increment=incrementValue $length=zeroPadLength $prefix=outputPrefix $intial=initialValue/>
-
-$tiddler defaults to <<currentTiddler>> 
-$field defaults to make_sure_you_give_a_field_parameter
-$increment defaults to 1
-$length has no default, if no length is given than the output will have no zero padding
-$prefix has no default, if no prefix is given the output isn't given a prefix
-$initial defaults to 0
-
-Putting:
-
-<$button>Test
-<$action-increment/>
-</$button>
-
-into a blank tiddler will, when the button is pressed the first time, make a field called make_sure_you_give_a_field_parameter and set the value to zero. Then each time the button is pressed the value in that field will increment by one.
-
-This will take the value of fieldToIncrement in someTiddler and increment it by incrementValue.
-
-The increment value can be negative or non-integer, the actual action is currentValue+incrementValue.
-
-Note to me: Look in boot.js to see if you can make this incremet times as well, by days, minutes, hours, etc.
-
 \*/
-
-
 
 (function(){
 
@@ -62,7 +37,7 @@ Compute the internal state of the widget
 */
 IncrementWidget.prototype.execute = function() {
 	this.actionTiddler = this.getAttribute("$tiddler",this.getVariable("currentTiddler"));
-	this.actionField = this.getAttribute("$field","make_sure_you_give_a_field_parameter");
+	this.actionField = this.getAttribute("$field","inc_field");
 	this.actionIndex = this.getAttribute("$index");
 	this.padLength = this.getAttribute("$length","0");
 	this.prefixValue = this.getAttribute("$prefix")
@@ -70,34 +45,32 @@ IncrementWidget.prototype.execute = function() {
 
 	var incVal = this.getAttribute("$increment",1);
 	var tiddler = this.wiki.getTiddler(this.actionTiddler);
-	var tempvalue = tiddler.getFieldString(this.actionField);
+	var fieldString = tiddler.getFieldString(this.actionField);
 
-	//Put a value in the field if the field is empty, with a prefix if a prefix is given
-	if ( tempvalue ) {
+	//If there is no value in the field create the initial value, with a prefix and zero padding if needed
+	if ( fieldString ) {
 	} else if ( this.prefixValue ) {
 		if ( this.padLength ) {
-			tempvalue = String(this.prefixValue)+String($tw.utils.pad(this.initialValue,this.padLength));
+			fieldString = this.prefixValue+$tw.utils.pad(this.initialValue,this.padLength);
 		} else {
-			tempvalue = String(this.prefixValue)+String(this.initialValue);
+			fieldString = this.prefixValue+this.initialValue;
 		}
 	} else if ( this.padLength ) {
-			tempvalue = $tw.utils.pad(this.initialValue,this.padLength);
+			fieldString = $tw.utils.pad(this.initialValue,this.padLength);
 	} else {
-			tempvalue = String(this.initialValue);
+			fieldString = this.initialValue;
 	}
 	
-	//Get the current value in the field, removing a prefix if one is given
+	//Get the current numeric value in the field by removing the prefix if one is given
 	if ( this.prefixValue ) {
-		var oldvalue = tempvalue.slice(this.prefixValue.length,tempvalue.length);
-	} else if ( tiddler.getFieldString(this.actionField) ) {
-		var oldvalue = tiddler.getFieldString(this.actionField);
+		var currentValue = fieldString.slice(this.prefixValue.length,fieldString.length);
 	} else {
-		var oldvalue = tempvalue;
+		var currentValue = fieldString;
 	}
 
-	//If either the existing value or the increment value are not numbers leave the field alone.
-	if ( !isNaN(parseFloat(oldvalue)) && isFinite(oldvalue) && !isNaN(parseFloat(incVal)) && isFinite(incVal) ) { 
-		var output = Number(oldvalue) + Number(incVal);
+	//If either the existing value or the increment value are not numbers leave the field alone, otherwise increment the value.
+	if ( !isNaN(parseFloat(currentValue)) && isFinite(currentValue) && !isNaN(parseFloat(incVal)) && isFinite(incVal) ) { 
+		var output = Number(currentValue) + Number(incVal);
 		if ( this.padLength ) {
 			this.actionValue = $tw.utils.pad(output,this.padLength)
 		} else {
@@ -107,7 +80,7 @@ IncrementWidget.prototype.execute = function() {
 			this.actionValue = String(this.prefixValue)+String(this.actionValue);
 		}
 	} else {
-		this.actionValue = String(oldvalue);
+		this.actionValue = String(currentValue);
 	}
 };
 
@@ -127,7 +100,6 @@ IncrementWidget.prototype.refresh = function(changedTiddlers) {
 Invoke the action associated with this widget
 */
 IncrementWidget.prototype.invokeAction = function(triggeringWidget,event) {
-	var self = this;
 	if(typeof this.actionValue === "string") {
 		this.wiki.setText(this.actionTiddler,this.actionField,this.actionIndex,this.actionValue);		
 	}
