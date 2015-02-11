7b730cbfbe167871e224391ec1dc0f08caea850b
inmysocks <inmysocks@gmail.com>
2015-02-10 20:47:45 -0700

    It works when you only have one expression tiddler
    I don't know why it doesn't work when there are multiple expression tiddlers.
    I think it may be because writing to the tiddler forces a refresh and the daemon loses its place and starts over.

--- sumfield-daemon.js
+++ sumfield-daemon.js
@@ -45,7 +45,7 @@ exports.startup = function() {
 					var expressionTiddler1 = $tw.wiki.getTiddler(expressionTiddlerList1[i]);
 					if(expressionTiddler1) {
 						if($tw.utils.hop(changes,expressionTiddlerList1[i])) {
-	//							sumField(expressionTiddler);
+//							sumField(expressionTiddler1);
 							sumFieldFull();
 						} else {
 							var inputFilter1 = expressionTiddler1.getFieldString("sum_filter","[is[system]!is[system]]");
@@ -54,7 +54,7 @@ exports.startup = function() {
 								for (var i = 0; i < tiddlerList1.length; i++) {
 									var tidtitle1 = tiddlerList1[i];
 									if($tw.utils.hop(changes,tidtitle1)) {
-	//										sumField(expressionTiddler);
+//										sumField(expressionTiddler1);
 										sumFieldFull();
 									}
 								}
@@ -67,6 +67,7 @@ exports.startup = function() {
 	});
 };
 
+/*
 function sumField(expressionTiddler) {
 	//Get parameters for current tiddler
 	var storeTiddler = expressionTiddler.getFieldString("sum_store_tiddler");
@@ -85,7 +86,7 @@ function sumField(expressionTiddler) {
 		    var tidtitle = tiddlerList[i];
 		    var tiddler = $tw.wiki.getTiddler(tidtitle);
 		    if(tiddler != undefined) {
-			    if ( !isNaN(parseFloat(tiddler.getFieldString(sumField))) && isFinite(tiddler.getFieldString(sumField)) ) {
+			    if(!isNaN(parseFloat(tiddler.getFieldString(sumField))) && isFinite(tiddler.getFieldString(sumField))) {
 		    		output = output + Number(tiddler.getFieldString(sumField));
 				}
 			}
@@ -97,6 +98,7 @@ function sumField(expressionTiddler) {
   		$tw.wiki.setText(storeTiddler,storeField,storeIndex,output);
   	}
 };
+*/
 
 function sumFieldFull() {
 	//Get the summation tag from the configuration tiddler
@@ -109,41 +111,39 @@ function sumFieldFull() {
 	//Iterate through the list of expression tidders and evaluate each one.
 	if(expressionTiddlerList.length != 0) {
 		for (var i = 0; i < expressionTiddlerList.length; i++) {
-			//Iterate through the list of expression tidders and evaluate each one.
-			if(expressionTiddlerList.length != 0) {
-				for (var i = 0; i < expressionTiddlerList.length; i++) {
-					var expressionTiddler = $tw.wiki.getTiddler(expressionTiddlerList[i]);
-					if(expressionTiddler) {
-						//Get parameters for current tiddler
-						var storeTiddler = expressionTiddler.getFieldString("sum_store_tiddler");
-						var storeField = expressionTiddler.getFieldString("sum_store_field");
-						var storeIndex = expressionTiddler.getFieldString("sum_store_index");
-						var sumField = expressionTiddler.getFieldString("sum_field");
-						var inputFilter = expressionTiddler.getFieldString("sum_filter","[is[system]!is[system]]");
-						var defaultValue = expressionTiddler.getFieldString("sum_default_value",0);
-						var tiddlerList = $tw.wiki.filterTiddlers(inputFilter);
-						// Check for an empty list, if the list isn't empty compute the sum
-						if(tiddlerList.length === 0) {
-						  	output = defaultValue; //return the default value when there is nothing to sum
-						} else {
-						  	var output = 0;
-						  	for (var i = 0; i < tiddlerList.length; i++) {
-							    var tidtitle = tiddlerList[i];
-							    var tiddler = $tw.wiki.getTiddler(tidtitle);
-							    if(tiddler != undefined) {
-								    if ( !isNaN(parseFloat(tiddler.getFieldString(sumField))) && isFinite(tiddler.getFieldString(sumField)) ) {
-							    		output = output + Number(tiddler.getFieldString(sumField));
-									}
-								}
-						 	}
+			var expressionTiddler = $tw.wiki.getTiddler(expressionTiddlerList[i]);
+			if(expressionTiddler) {
+				//Get parameters for current tiddler
+				var storeTiddler = expressionTiddler.getFieldString("sum_store_tiddler");
+				var storeField = expressionTiddler.getFieldString("sum_store_field");
+				var storeIndex = expressionTiddler.getFieldString("sum_store_index");
+				var sumField = expressionTiddler.getFieldString("sum_field");
+				var inputFilter = expressionTiddler.getFieldString("sum_filter","[is[system]!is[system]]");
+				var defaultValue = expressionTiddler.getFieldString("sum_default_value",0);
+				var tiddlerList = $tw.wiki.filterTiddlers(inputFilter);
+				// Check for an empty list, if the list isn't empty compute the sum
+				if(tiddlerList.length === 0) {
+				  	output = defaultValue; //return the default value when there is nothing to sum
+				} else {
+				  	var output = 0;
+				  	for (var i = 0; i < tiddlerList.length; i++) {
+					    var tidtitle = tiddlerList[i];
+					    var tiddler = $tw.wiki.getTiddler(tidtitle);
+					    if(tiddler != undefined) {
+						    if(!isNaN(parseFloat(tiddler.getFieldString(sumField))) && isFinite(tiddler.getFieldString(sumField))) {
+					    		output = output + Number(tiddler.getFieldString(sumField));
+							}
 						}
-					    //If the output isn't blank then write to the field
-						output = String(output);
-						if(output != '') {
-					  		$tw.wiki.setText(storeTiddler,storeField,storeIndex,output);
-					  	}
-					}
+				 	}
 				}
+				var checkTiddler = $tw.wiki.getTiddler(storeTiddler);
+				if(checkTiddler && storeField) {
+				    //If the output different than the current value, write the new value
+					output = String(output);
+					if(output != checkTiddler.getFieldString(storeField)) {
+				  		$tw.wiki.setText(storeTiddler,storeField,storeIndex,output);
+				  	}
+			  	}
 			}
 		}
 	}
